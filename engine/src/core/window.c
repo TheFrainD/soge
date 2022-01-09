7e7d93dccbd84b65c842587267bcadad6c4f1fee
Dmytro Zykov <dmytri.zykov@gmail.com>
2022-01-09 07:44:08 +0200

    Fix projection Projection and OpenGL canvas did not change after window resizing

--- engine/src/core/window.c
+++ engine/src/core/window.c
@@ -1,9 +1,10 @@
-#include <core/window.h>
+#include "core/window.h"
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <core/logger.h>
+#include "core/logger.h"
+#include "core/event.h"
 
 typedef struct {
   GLFWwindow *window;
@@ -14,6 +15,16 @@ typedef struct {
 
 static b8 initialized = FALSE;
 static window_state state;
+
+static void window_size_callback(GLFWwindow *window, i32 width, i32 height) {
+  state.width = width;
+  state.height = height;
+  glViewport(0, 0, width, height);
+  event_context context;
+  context.data.f32[0] = (float)width;
+  context.data.f32[1] = (float)height;
+  event_publish(EVENT_CODE_WINDOW_RESIZED, &state, context);
+}
 
 b8 window_create(i32 width, i32 height, const char* title) {
   if (initialized) {
@@ -41,6 +52,8 @@ b8 window_create(i32 width, i32 height, const char* title) {
   VALLY_TRACE("Window created");
   glfwMakeContextCurrent(state.window);
   glfwSwapInterval(1);
+
+  glfwSetWindowSizeCallback(state.window, window_size_callback);
 
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     VALLY_FATAL("Could not initialize glad!");
