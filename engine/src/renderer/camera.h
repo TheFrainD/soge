95357788a7923346c27e8116dd9613207b0a84fd
Dmytro Zykov <dmytri.zykov@gmail.com>
2022-01-10 22:55:43 +0200

    Make new example game

--- engine/src/renderer/camera.h
+++ engine/src/renderer/camera.h
@@ -17,6 +17,6 @@ mat4s camera_update_projection(i32 width, i32 height);
 mat4s camera_update_view();
 mat4s camera_get_projection();
 mat4s camera_get_view();
-void camera_translate(vec2s translation);
+VALLY_DLL void camera_translate(vec2s translation);
 
 #endif // !VALLY_RENDERER_CAMERA_H_
--- sandbox/src/game.c
+++ sandbox/src/game.c
@@ -1,40 +1,57 @@
 #include <core/engine.h>
 #include <core/input.h>
 #include <ecs/ecs.h>
-#include <ecs/components/spriterenderer.h>
-#include <ecs/components/transform.h>
 #include <math/vmath.h>
+#include <renderer/camera.h>
 
-entity mario;
-entity toad;
-entity stone;
+entity knight;
+animation run_right;
+animation run_left;
 
-texture *terrain;
-animation anim;
+entity stones[16];
+
+f32 speed = 120.0f;
 
 void start() {
-  mario = ecs_entity_create();
-  ecs_spriterenderer_add(mario, texture_create(image_load("../assets/textures/mario.png")));
-  ecs_transform_get(mario)->scale = vector2f_create(0.25f, 0.25f);
+  run_right = animation_create(texture_create(image_load("../assets/textures/knight_right.png")), 
+  vector2f_create(16.0f, 16.0f), 0, 5, 12);
 
-  toad = ecs_entity_create();
-  ecs_spriterenderer_add(toad, texture_create(image_load("../assets/textures/toad.png")));
-  ecs_transform_get(toad)->scale = vector2f_create(0.1f, 0.1f);
+  run_left = animation_create(texture_create(image_load("../assets/textures/knight_left.png")), 
+  vector2f_create(16.0f, 16.0f), 0, 5, 12);
 
-  terrain = texture_create(image_load("../assets/textures/terrain.png"));
-  anim = animation_create(terrain, vector2f_create(16.0f, 16.0f), 0, 4, 1);
+  texture *terrain = texture_create(image_load("../assets/textures/terrain.png"));
+  for (i32 i = 0; i < 16; i++) {
+    stones[i] = ecs_entity_create();
+    ecs_spriterenderer_add_from_atlas(stones[i], terrain, vector2f_create(16.0f, 16.0f));
+    ecs_spriterenderer_atlas_select(stones[i], 1);
+    ecs_transform_get(stones[i])->scale = vector2f_create(4.0f, 4.0f);
+    ecs_transform_get(stones[i])->position = vector2f_create(0.0f + 64.0f * i, 180.0f);
+  }
 
-  stone = ecs_entity_create();
-  ecs_spriterenderer_add_from_atlas(stone, terrain, vector2f_create(16.0f, 16.0f));
-  ecs_animator_add(stone);
-  ecs_animator_play(stone, &anim);
-  //ecs_spriterenderer_atlas_select(stone, 4);
-  ecs_transform_get(stone)->scale = vector2f_create(8.0f, 8.0f);
-  ecs_transform_get(stone)->position = vector2f_create(64.0f, 64.0f);
+  knight = ecs_entity_create();
+  ecs_spriterenderer_add_from_atlas(knight, texture_create(image_load("../assets/textures/knight_right.png")), vector2f_create(16.0f, 16.0f));
+  ecs_animator_add(knight);
+  ecs_transform_get(knight)->position = vector2f_create(640.0f / 2.0f - 32.0f, 126.0f);
+  ecs_transform_get(knight)->scale = vector2f_create(4.0f, 4.0f);
 }
 
 b8 update(f32 dt) {
-  ecs_transform_get(mario)->position.x += 20.0f * dt;
+  f32 translation = 0.0f;
+
+  if (input_key_pressed(KEY_A)) {
+    translation = -speed * dt;
+    ecs_animator_play(knight, &run_left);
+  } else if (input_key_pressed(KEY_D)) {
+    translation = speed * dt;
+    ecs_animator_play(knight, &run_right);
+  } else {
+    ecs_animator_stop(knight);
+  }
+
+  ecs_transform_get(knight)->position.x += translation;
+  if (translation != 0.0f) {
+    camera_translate((vec2s){translation, 0.0f});
+  }
 }
 
 b8 render(f32 dt) {
